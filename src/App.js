e58bef188abeda6654cca5e797ec095cbf66b375
Kira Corbett <kiracorbett00@gmail.com>
2023-02-05 14:58:29 -0800

    Init photo gallery

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
+import Gallery from "react-photo-gallery";
+import { photos } from "./photos";
 import './App.css';
 
 function App() {
+  const BasicRows = () => <Gallery photos={photos} />;
+
   return (
     <div className="App">
-      
+      <BasicRows />, document.getElementById("app")
     </div>
   );
 }
